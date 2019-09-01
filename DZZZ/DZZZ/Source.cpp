e1edf4efdf84d81e4a830870324dd1928b4bde04
Tania Tushkyn <tsntania@gmail.com>
2019-09-01 23:48:18 +0300

    pointer

---
+++ DZZZ/DZZZ/Source.cpp
@@ -0,0 +1,233 @@
+﻿#include<iostream>
+#include<ctime>
+
+using namespace std;
+
+
+//cout << "На шоу Пошук Зірок судді кожному учаснику виставляють оцінку від 0 до";
+//cout << "10(оцінка може бути дробовим числом).Знайти підсумкову оцінку кожного";
+//cout << "учасника, яка обчислюється як середнє арифметичне, але без врахування найкращої";
+//cout << "та найгіршої оцінок.Виконати задачу з розбиттям на функції";
+//
+//void Fill(double mas[], const int SIZE);
+//
+//double MAX(double mas[], const int SIZE);
+//
+//double MIN(double mas[], const int SIZE);
+//
+//double SER(double mas[], const int SIZE, double max, double min, double Suma, int kil, double Ser);
+//
+//int main()
+//{
+//	const int SIZE = 5;
+//	double mas[SIZE];
+//	double max = 0;
+//	double min =0;
+//	double Suma = 0;
+//	double Ser = 0;
+//	int kil = 0;
+//	const int kilParticipant = 5;
+//
+//	for (int i = 0; i < kilParticipant; i++)
+//	{
+//		cout << "\n             *************** Search Stars *****************\n\n";
+//		cout << "<------------- participant "<<i+1<<" ----------------->\n";
+//		Fill(mas, SIZE);
+//		cout << "---------------------------------------------------\n";
+//		max = MAX(mas, SIZE);
+//		cout << "           max"<<i+1<<" = " << max << endl;
+//
+//		min = MIN(mas, SIZE);
+//		cout << "           min"<<i+1<<" = " << min << endl;
+//
+//
+//		Ser = SER(mas, SIZE, max, min, Suma, kil, Ser);
+//		cout << "<<==========================================>>\n";
+//		cout << "           Ser"<<i+1<<" = " << Ser << endl;
+//		cout << "<<==========================================>>\n\n\n";
+//	}
+//
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//
+//
+//
+//void Fill(double mas[], const int SIZE)
+//{
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		cout << "judge " << i + 1 << "      -->>   ";
+//		cin >> mas[i];
+//		if (mas[i] < 0 || mas[i] > 10)
+//		{
+//			cout << "                              -------- Enter 0 to 10           \n";
+//			cout << "judge " << i + 1 << "      -->>   ";
+//			cin >> mas[i];
+//
+//		}
+//		
+//	}
+//}
+//
+//
+//
+//double MAX(double mas[], const int SIZE)
+//{
+//	double max = mas[0];
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		if (mas[i] >= max)
+//		{
+//			max = mas[i];
+//		}
+//
+//	}
+//	return max;
+//}
+//
+//
+//
+//
+//double MIN(double mas[], const int SIZE)
+//{
+//	double min = mas[0];
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		if ( mas[i]<= min)
+//		{
+//			min = mas[i];
+//		}
+//
+//	}
+//	return min;
+//}
+//
+//
+//double SER(double mas[], const int SIZE, double max, double min, double Suma, int kil, double Ser)
+//{
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		if (mas[i] != min && mas[i] != max)
+//		{
+//			Suma += mas[i];
+//			kil++;
+//		}
+//
+//	}
+//	Ser = Suma / kil;
+//	return Ser;
+//}
+//
+//
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+//
+//cout << "Дано 2 паралельних одновимірних масиви : масив назв товарів(масив рядків)";
+//cout << "та масив цін товарів.Впорядкувати масиви за зростанням цін.Вивести назви";
+//cout << "та ціни товарів за заростанням та спаданням цін.Використати алгоритм";
+//cout << "впорядкування методом бульбашки.";
+
+
+void Fill(char masName[], const int SIZE2, const int SIZE);
+
+void FillPrice(double masPrice[], const int SIZE2);
+
+void Sort(double masPrice[], const int SIZE2);
+
+void Print(double masPrice[], const int SIZE2, char masName[], const int SIZE);
+
+int main()
+{
+	const int SIZE = 50;
+	const int SIZE2 = 5;
+	char masName[SIZE];
+	double masPrice[SIZE2];
+
+	cout << "Enter 5 of name --> \nFor end enter '0'\n";
+	Fill(masName, SIZE, SIZE2);
+	cout << "\n===============================================>>>\n";
+	FillPrice(masPrice, SIZE2);
+
+
+
+	 Sort( masPrice,  SIZE2);
+
+	  Print( masPrice,  SIZE2,  masName, SIZE);
+	system("pause");
+	return 0;
+}
+
+void Fill(char masName[], const int SIZE2, const int SIZE)
+{
+
+	for (int i = 0; i < SIZE2; i++)
+	{
+		
+		cin >> masName[i];
+		if (masName[i] == '0')
+		{
+			break;
+		}
+
+
+	}
+}
+
+
+
+void FillPrice(double masPrice[], const int SIZE2)
+{
+	for (int i = 0; i < SIZE2; i++)
+	{
+		cout << "Enter price --- ";
+		cin >> masPrice[i];
+
+	}
+}
+
+
+
+
+
+
+void Sort(double masPrice[], const int SIZE2)
+{
+	int tmp = 0;
+	for (int j = 0; j < SIZE2 - 1; j++) {
+		for (int i = 0; i < SIZE2 - 1; i++) {
+			if (masPrice[i] > masPrice[i + 1]) {
+				tmp = masPrice[i];
+				masPrice[i] = masPrice[i + 1];
+				masPrice[i + 1] = tmp;
+			}
+		}
+	}
+}
+
+
+
+
+
+void Print(double masPrice[], const int SIZE2, char masName[], const int SIZE)
+{
+	for (int i = 0; i < SIZE2; i++)
+	{
+		cout << " masPrice Sort [" << i << "] = " << masPrice[i] << endl;
+		cout << " masName Sort [" << i << "] = " << masName[i] <<endl;
+
+	}
+}
+
+
+
+
---
+++ test/test/Source.cpp
@@ -0,0 +1,265 @@
+﻿#include<iostream>
+#include<ctime>
+
+using namespace std;
+
+
+//1. Дано масив цілих чисел.Користуючись вказівниками, 
+//поміняти місцями елементи масиву з парними и непарними
+//індексами(тобто ті елементи масиву, які стоять на парних місцях, 
+//поміняти з елементами, які стоять на непарних місцях).
+
+//
+//void Fill(int mas[], const int *pSIZE);
+//
+//void Print(int mas[], const int *pSIZE);
+//
+//void Mix(int mas[], const int *pSIZE, int *pTmp);
+//
+//int main()
+//{
+//	srand(unsigned(time(NULL)));
+//	const int SIZE = 6;
+//	const int *pSIZE = &SIZE;
+//	int mas[SIZE];
+//	int tmp = 0;
+//	int *pTmp = &tmp;
+//
+//	 Fill( mas, pSIZE);
+//	 Print( mas, pSIZE);
+//
+//	cout << "<<===================================================>>\n";
+//
+//	 Mix( mas, pSIZE,  pTmp);
+//	 Print(mas, pSIZE);
+//
+//	system("pause");
+//	return 0;
+//
+//
+//}
+//
+//void Fill(int mas[], const int *pSIZE)
+//{
+//	for (int i = 0; i < *pSIZE; i++)
+//	{
+//		mas[i] = rand() % 20 + -5;
+//	}
+//}
+//
+//
+//
+//void Print(int mas[], const int *pSIZE)
+//{
+//	for (int i = 0; i < *pSIZE; i++)
+//	{
+//		cout << "mas [" << i << "] = " << mas[i] << endl;
+//	}
+//}
+//
+//void Mix(int mas[], const int *pSIZE, int *pTmp)
+//{
+//	for (int i = 0; i < *pSIZE; i++)
+//	{
+//		if (i % 2 != 0)
+//		{
+//			*pTmp = mas[i];
+//			mas[i] = mas[i - 1];
+//			mas[i - 1] = *pTmp;
+//		}
+//	}
+//}
+
+
+
+
+
+//----------------------------------------------------------------------------
+
+
+
+
+//
+//2. Дано два масиви, впорядкованих по зростанню : А[n] і B[m].
+//Сформуйте масив C[n + m], який складається з елементів масивів А і В,
+//впорядкованих по зростанню.
+
+
+
+void Fill1(int mas1[], const int *pSIZE1);
+void Print1(int mas1[], const int *pSIZE1);
+void Sort1(int mas1[], const int *pSIZE1, int *pTmp);
+
+void Fill2(int mas2[], const int *pSIZE2);
+void Print2(int mas2[], const int *pSIZE2);
+void Sort2(int mas2[], const int *pSIZE2, int *pTmp);
+
+void FillMasC(int mas3[], int mas2[], int mas1[], const int *pSIZE1, const int *pSIZE2);
+void PrintMasC(int mas3[], int mas2[], int mas1[], const int *pSIZE1, const int *pSIZE2);
+
+void SortMasC(int mas3[], const int *pSIZE1, const int *pSIZE2, int *pTmp);
+void PrintSortMasC(int mas3[], const int *pSIZE1, const int *pSIZE2);
+
+int main()
+{
+	srand(unsigned(time(NULL)));
+	const int SIZE1 = 5;
+	const int *pSIZE1 =&SIZE1;
+	const int SIZE2 = 10;
+	const int *pSIZE2 = &SIZE2;
+	int tmp = 0;
+	int *pTmp = &tmp;
+
+	int mas1[SIZE1];
+	int mas2[SIZE2];
+	int mas3[SIZE1 + SIZE2];
+
+
+	cout << "================ Mas 1 ================\n";
+	 Fill1( mas1,  pSIZE1);
+	 Print1( mas1,  pSIZE1);
+	 cout << "============== Mas 1 Sort ============\n";
+	  Sort1( mas1, pSIZE1,  pTmp);
+	  Print1(mas1, pSIZE1);
+
+	  cout << "================ Mas 2 ================\n";
+
+	 Fill2( mas2,  pSIZE2);
+	 Print2(mas2, pSIZE2);
+
+	 cout << "============== Mas 2 Sort ============\n";
+
+	  Sort2( mas2,  pSIZE2,  pTmp);
+	  Print2(mas2, pSIZE2);
+
+
+	  cout << "============== Mas C =================\n";
+
+	   FillMasC( mas3,  mas2,  mas1, pSIZE1,  pSIZE2);
+	   PrintMasC( mas3,  mas2,  mas1,   pSIZE1,  pSIZE2);
+
+	   cout << "\n\n\n================================ Mas C Sort  ===================================\n";
+
+
+	    SortMasC( mas3, pSIZE1,  pSIZE2,  pTmp);
+		 PrintSortMasC( mas3,  pSIZE1, pSIZE2);
+		 cout << "=================================================================================\n";
+
+	system("pause");
+	return 0;
+}
+
+
+void Fill1(int mas1[], const int *pSIZE1)
+{
+	for (int i = 0; i < *pSIZE1; i++)
+	{
+		mas1[i] = rand() % 15 + -5;
+	}
+}
+
+
+
+void Print1(int mas1[], const int *pSIZE1)
+{
+	for (int i = 0; i < *pSIZE1; i++)
+	{
+		cout << "--- mas1[" << i << "] = " << mas1[i] << endl;
+	}
+}
+
+
+
+void Sort1(int mas1[], const int *pSIZE1, int *pTmp)
+{
+	for (int j = 0; j < *pSIZE1 - 1; j++) {
+		for (int i = 0; i < *pSIZE1 - 1; i++) {
+			if (mas1[i] > mas1[i + 1]) {
+				*pTmp = mas1[i];
+				mas1[i] = mas1[i + 1];
+				mas1[i + 1] = *pTmp;
+			}
+		}
+	}
+}
+
+void Fill2(int mas2[], const int *pSIZE2)
+{
+	for (int i = 0; i < *pSIZE2; i++)
+	{
+		mas2[i] = rand() % 15 + -5;
+	}
+}
+
+
+
+void Print2(int mas2[], const int *pSIZE2)
+{
+	for (int i = 0; i < *pSIZE2; i++)
+	{
+		cout << "--- mas2[" << i << "] = " << mas2[i] << endl;
+	}
+}
+
+
+void Sort2(int mas2[], const int *pSIZE2, int *pTmp)
+{
+	for (int j = 0; j < *pSIZE2-1; j++) {
+		for (int i = 0; i < *pSIZE2-1; i++) {
+			if (mas2[i] > mas2[i + 1]) {
+				*pTmp = mas2[i];
+				mas2[i] = mas2[i + 1];
+				mas2[i + 1] = *pTmp;
+			}
+		}
+	}
+}
+
+
+
+
+void FillMasC(int mas3[], int mas2[], int mas1[], const int *pSIZE1, const int *pSIZE2)
+{
+	for (int i = 0; i < *pSIZE1 ; i++)
+	{
+		mas3[i] = mas1[i];
+	}
+
+
+	for (int i = *pSIZE1; i < *pSIZE2+ *pSIZE1; i++)
+	{
+		mas3[i] = mas2[i-*pSIZE1];
+	}
+}
+
+void PrintMasC(int mas3[], int mas2[], int mas1[], const int *pSIZE1, const int *pSIZE2)
+{
+	for (int i = 0; i < *pSIZE1 + *pSIZE2; i++)
+	{
+		cout << "MasC [" << i << "] = " << mas3[i] << endl;
+	}
+}
+
+
+void SortMasC(int mas3[], const int *pSIZE1, const int *pSIZE2,  int *pTmp)
+{
+	for (int j = 0; j < (*pSIZE2+ *pSIZE1) - 1; j++) {
+		for (int i = 0; i < (*pSIZE2 + *pSIZE1) - 1; i++) {
+			if (mas3[i] > mas3[i + 1]) {
+				*pTmp = mas3[i];
+				mas3[i] = mas3[i + 1];
+				mas3[i + 1] = *pTmp;
+			}
+		}
+	}
+}
+
+
+
+void PrintSortMasC(int mas3[], const int *pSIZE1, const int *pSIZE2 )
+{
+	for (int i = 0; i < *pSIZE2 + *pSIZE1; i++)
+	{
+		cout << "                 Mas C Sort [" << i << "] = " << mas3[i] << endl;
+	}
+}
